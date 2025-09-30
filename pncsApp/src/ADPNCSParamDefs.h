01262335b4fca584474f18aaa954e2a8c7b64ab2
Jakub Wlodek <jwlodek@bnl.gov>
2025-09-30 09:38:06 -0400

    Modify naming for param strings

--- pncsApp/src/ADPNCSParamDefs.h
+++ pncsApp/src/ADPNCSParamDefs.h
@@ -5,45 +5,45 @@
 // Generated from ADPNCS.template
 
 // String definitions for parameters
-#define ADPNCS_TemperatureSetpointString "CXC_TEMPERATURE_SETPOINT"
-#define ADPNCS_LiveRunningCorrectionString "CXC_LIVE_RUNNING_CORRECTION"
-#define ADPNCS_VoltageStateString "CXC_VOLTAGE_STATE"
-#define ADPNCS_CoolingStateString "CXC_COOLING_STATE"
-#define ADPNCS_SecondaryThresholdString "CXC_SECONDARY_THRESHOLD"
-#define ADPNCS_PowerStateString "CXC_POWER_STATE"
-#define ADPNCS_CalibrateString "CXC_CALIBRATE"
-#define ADPNCS_HeatsinkTemperatureString "CXC_HEATSINK_TEMPERATURE"
-#define ADPNCS_PrimaryThresholdString "CXC_PRIMARY_THRESHOLD"
-#define ADPNCS_DynamicCalibrationString "CXC_DYNAMIC_CALIBRATION"
-#define ADPNCS_CommonModeCorrectionString "CXC_COMMON_MODE_CORRECTION"
-#define ADPNCS_StateString "CXC_STATE"
-#define ADPNCS_CameraModeString "CXC_CAMERA_MODE"
-#define ADPNCS_HeatingStateString "CXC_HEATING_STATE"
-#define ADPNCS_BeamBlankStateString "CXC_BEAM_BLANK_STATE"
-#define ADPNCS_ConnectionStatusString "CXC_CONNECTION_STATUS"
-#define ADPNCS_TemperatureString "CXC_TEMPERATURE"
+#define ADPNCS_BeamBlankStateString "PNCS_BEAM_BLANK_STATE"
+#define ADPNCS_HeatsinkTemperatureString "PNCS_HEATSINK_TEMPERATURE"
+#define ADPNCS_LiveRunningCorrectionString "PNCS_LIVE_RUNNING_CORRECTION"
+#define ADPNCS_TemperatureString "PNCS_TEMPERATURE"
+#define ADPNCS_HeatingStateString "PNCS_HEATING_STATE"
+#define ADPNCS_CalibrateString "PNCS_CALIBRATE"
+#define ADPNCS_CameraModeString "PNCS_CAMERA_MODE"
+#define ADPNCS_CommonModeCorrectionString "PNCS_COMMON_MODE_CORRECTION"
+#define ADPNCS_ConnectionStatusString "PNCS_CONNECTION_STATUS"
+#define ADPNCS_PowerStateString "PNCS_POWER_STATE"
+#define ADPNCS_CoolingStateString "PNCS_COOLING_STATE"
+#define ADPNCS_VoltageStateString "PNCS_VOLTAGE_STATE"
+#define ADPNCS_PrimaryThresholdString "PNCS_PRIMARY_THRESHOLD"
+#define ADPNCS_TemperatureSetpointString "PNCS_TEMPERATURE_SETPOINT"
+#define ADPNCS_SecondaryThresholdString "PNCS_SECONDARY_THRESHOLD"
+#define ADPNCS_DynamicCalibrationString "PNCS_DYNAMIC_CALIBRATION"
+#define ADPNCS_StateString "PNCS_STATE"
 
 // Parameter index definitions
-int ADPNCS_TemperatureSetpoint;
-int ADPNCS_LiveRunningCorrection;
-int ADPNCS_VoltageState;
-int ADPNCS_CoolingState;
-int ADPNCS_SecondaryThreshold;
-int ADPNCS_PowerState;
-int ADPNCS_Calibrate;
-int ADPNCS_HeatsinkTemperature;
-int ADPNCS_PrimaryThreshold;
-int ADPNCS_DynamicCalibration;
-int ADPNCS_CommonModeCorrection;
-int ADPNCS_State;
-int ADPNCS_CameraMode;
-int ADPNCS_HeatingState;
 int ADPNCS_BeamBlankState;
-int ADPNCS_ConnectionStatus;
+int ADPNCS_HeatsinkTemperature;
+int ADPNCS_LiveRunningCorrection;
 int ADPNCS_Temperature;
+int ADPNCS_HeatingState;
+int ADPNCS_Calibrate;
+int ADPNCS_CameraMode;
+int ADPNCS_CommonModeCorrection;
+int ADPNCS_ConnectionStatus;
+int ADPNCS_PowerState;
+int ADPNCS_CoolingState;
+int ADPNCS_VoltageState;
+int ADPNCS_PrimaryThreshold;
+int ADPNCS_TemperatureSetpoint;
+int ADPNCS_SecondaryThreshold;
+int ADPNCS_DynamicCalibration;
+int ADPNCS_State;
 
-#define ADPNCS_FIRST_PARAM ADPNCS_TemperatureSetpoint
-#define ADPNCS_LAST_PARAM ADPNCS_Temperature
+#define ADPNCS_FIRST_PARAM ADPNCS_BeamBlankState
+#define ADPNCS_LAST_PARAM ADPNCS_State
 
 #define NUM_ADPNCS_PARAMS ((int)(&ADPNCS_LAST_PARAM - &ADPNCS_FIRST_PARAM + 1))
 
