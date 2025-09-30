d98aeade9afd7b0a462dbb6b165b1d432c9c56d5
Jakub Wlodek <jwlodek@bnl.gov>
2025-09-30 09:44:44 -0400

    Minor fixes to naming

--- iocs/pncsIOC/iocBoot/iocPNCS/st.cmd
+++ iocs/pncsIOC/iocBoot/iocPNCS/st.cmd
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# Export library path in case building against shared libraries
-export LD_LIBRARY_PATH=../../../../lib/linux-x86_64:/opt/grpc/lib:/opt/grpc/lib64:/opt/pncs_sdk/lib:$LD_LIBRARY_PATH
-
-../../bin/linux-x86_64/PNDetectorCXCApp st_base.cmd
+../../bin/linux-x86_64/pncsApp st_base.cmd
